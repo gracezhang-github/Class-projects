462cd5a8ca9a019b4d77e657e5403191d1ea12da
gracezhang-github <59672874+gracezhang-github@users.noreply.github.com>
2021-02-10 14:42:09 -0800

    Add files via upload

---
+++ GuitarHero/Guitar37.java
@@ -0,0 +1,104 @@
+// Grace Zhang
+// 01/20/2021
+// CSE143
+// TA:  Melody Lam
+// Take-home Assessment #2
+// 
+// This program is a different implementation of the Guitar interface,
+// this will model a guitar with 37 different strings.
+
+import java.util.*;
+
+public class Guitar37 implements Guitar {
+    public static final String KEYBOARD =
+        "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";  // keyboard layout
+
+    private GuitarString[] strings;
+    private int ticNum;
+    
+    /*
+     * Constructs an implementation of the Guitar Interface,
+     *      creates GuitarString object with 37 strings on the
+     *      chromatic scale from 110Hz to 880Hz
+    */ 
+    public Guitar37() {
+        ticNum = 0;
+        strings = new GuitarString[KEYBOARD.length()];
+        for (int i = 0; i < KEYBOARD.length(); i++) {
+            strings[i] = new GuitarString(440.0 * Math.pow(2, ((i - 24.0) / 12.0)));
+        }
+    }
+
+    /*
+     * Plays a given note by passing in a pitch, where illegal
+     *      pitches will be ignored. In this case, the allowed 
+     *      legal pitch range will be from 110Hz to 880Hz
+     * @param pitch - an integer where the value 0 represents
+     *                concert-A and all other notes are specified
+     *                relative to concert A using a chromatic scale.
+     *                Illegal pitches will be ignored, legal pitches
+     *                are classified between 110Hz - 880 Hz
+    */ 
+    public void playNote(int pitch) {
+        int index = pitch + 24;
+        if ((index < KEYBOARD.length()) && (index >= 0)) {
+            strings[index].pluck();
+        } 
+    }
+
+    /*
+     * Verifies if a particular input key in the guitar
+     * @param key - the inputted key to verify
+     * @return - returns true if the key is in the guitar,
+     *           return false if not
+    */ 
+    public boolean hasString(char key) {
+        return KEYBOARD.indexOf(key) != -1;
+    }
+
+    /*
+     * Checks if the key is legal for the guitar
+     * Decides which note to play with the specified character
+     *      given by client and then produce the appropriate output 
+     * @throws - IllegalArgumentException if the key is not one of the 
+     *           37 keys it's designed to play/checks if the key is legal
+     * @param key - the inputted key to check for legality and to play the
+     *              appropriate output
+    */ 
+    public void pluck(char key) {
+        if (!hasString(key)) {
+            throw new IllegalArgumentException();
+        }
+        strings[KEYBOARD.indexOf(key)].pluck();
+    }
+
+    /*
+     * Retrieves the current sound sumple
+     * @return - the total current sound sample
+    */ 
+    public double sample() {
+        double sampleSum = 0.0;
+        for (int i = 0; i < KEYBOARD.length(); i++) {
+            sampleSum += strings[i].sample();
+        }
+        return sampleSum;
+    }
+
+    /*
+     * Advances the time forward one tic
+    */ 
+    public void tic() {
+        for (int i = 0; i < KEYBOARD.length(); i++) {
+            strings[i].tic();
+        }
+        ticNum++;
+    }
+    /*
+     * Determines the current time - number of times tic has been called
+     * @return - the total number of times tic was called
+    */ 
+    public int time() {
+        return ticNum;
+    }
+
+}
---
+++ GuitarHero/GuitarString.java
@@ -0,0 +1,80 @@
+// Grace Zhang
+// 01/20/2021
+// CSE143
+// TA:  Melody Lam
+// Take-home Assessment #2
+// 
+// This program models a vibrating guitar string of a given frequency.
+
+import java.util.*;
+
+public class GuitarString {
+    public static final double ENERGY_DECAY = 0.996;
+    private Queue<Double> ringBuffer;
+    
+    /*
+     * Constructs a GuitarString with the given frequency 
+     * This will initialize the ring buffer to represent a guitar
+     * string at rest by filling the ring buffer with zeros.
+     * @throws - IllegalArgumentException if the frequnecy is less than
+     *           or equal to 0 and if the capacity of the ringbuffer is
+     *           less than 2
+     * @param frequency - the given frequency; initializes the ring buffer
+     *                    with all zeros to represent that the string is at rest
+    */ 
+    public GuitarString(double frequency) {
+        double capacityN = (int) (Math.round(StdAudio.SAMPLE_RATE / frequency));
+        if (frequency <= 0 || capacityN < 2) {
+            throw new IllegalArgumentException();
+        }
+        ringBuffer = new LinkedList<Double>();
+        for (int i = 0; i < capacityN; i++) {
+            ringBuffer.add(0.0);
+        }
+    }
+
+    /*
+     * Constructs a GuitarString and initializes the contents of the ring buffer
+     * @throws - IllegalArgumentException if the size of the given values is less
+     *           than 2
+     * @param init - the inital values given; initializes the ring buffer to be filled 
+     *               with init's contents
+    */ 
+    public GuitarString(double[] init) {
+        if (init.length < 2) {
+            throw new IllegalArgumentException();
+        }
+        ringBuffer = new LinkedList<Double>();
+        for (int i = 0; i < init.length; i++) {
+            ringBuffer.add(init[i]);
+        }
+    }
+
+    /*
+     * Replaces the ring buffer with white noise
+    */ 
+    public void pluck() {
+        for (int i = 0; i < ringBuffer.size(); i++) {
+            ringBuffer.add(Math.random() - 0.5);
+            ringBuffer.remove();
+        }
+    }
+
+    /*
+     * Updates the simulation once through the Karplus-Strong Synthesis
+    */ 
+    public void tic() {
+        double front = ringBuffer.remove();
+        double second = ringBuffer.peek();
+        double average = ((front + second) / 2) * ENERGY_DECAY;
+        ringBuffer.add(average);
+    }
+
+    /*
+     * Returns the current sample
+     * @return - returns the current sample
+    */ 
+    public double sample() {
+        return ringBuffer.peek();
+    }
+}
