026ee9fb5fc122f326afd2023ab25e31a7e9f3fc
gracezhang-github <59672874+gracezhang-github@users.noreply.github.com>
2021-02-10 14:36:50 -0800

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,4 +1,4 @@
- // This program allows the user to play a game in
+// This program allows the user to play a game in
 // which the program thinks of a random integer
 // and accepts guesses from the user until the user
 // guesses the number correct.
